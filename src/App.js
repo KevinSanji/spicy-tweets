694303b3b6120dfb120f5ba405e108f3a39e8d3b
Kevin Sanji <kevinrsanji@gmail.com>
2018-12-19 23:26:00 -0800

    add search functionality

--- src/App.js
+++ src/App.js
@@ -11,7 +11,19 @@ class App extends Component {
   }
 
   searchUser () {
-    console.log('hello');
+    console.log(this);
+    const BASE_URL = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+    const USER = this.state.user;
+    let FETCH_URL = `${BASE_URL}?screen_name=${USER}&count=3`;
+    console.log(USER);
+    fetch(FETCH_URL)
+    .then(response => response.json())
+    .then(json => {
+      console.log(json);
+    })
+    .catch(err => {
+      console.log('Fetch error', err);
+    })
   }
 
   render() {
@@ -19,7 +31,7 @@ class App extends Component {
       <div className='app'>
         <h1>Spicy Tweets</h1>
         <input type='text' onChange={event => this.setState({user: event.target.value})} placeholder='Search User'/>
-        <button onClick={this.searchUser}>Search</button>
+        <button onClick={() => this.searchUser()}>Search</button>
         <h2>{this.state.user}'s Spiciest Tweets</h2>
         <div className='tweet-container'>
           <ul>
